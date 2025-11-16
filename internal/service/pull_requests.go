f3789bc445513845b9ebdef389e72f2467531b86
Elena Starostina <starostina.lena05@gmail.com>
2025-11-16 18:30:53 +0300

    pr assign logic

--- internal/service/pull_requests.go
+++ internal/service/pull_requests.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"math/rand"
+	"time"
 )
 
 type PullRequestService interface {
@@ -26,7 +28,25 @@ func (s *PullRequestServiceImpl) CreatePullRequest(ctx context.Context, title st
 	if title == "" {
 		return 0, errors.New("title required")
 	}
-	return s.storage.AddPullRequest(ctx, title, authorId, isOpened, nil, nil)
+
+	var reviewer1 *int64
+	var reviewer2 *int64
+	if isOpened {
+		coworkers, err := s.storage.GetActiveCoworkers(ctx, authorId)
+		if err == nil {
+			if len(coworkers) == 1 {
+				reviewer1 = &(coworkers[0].ID)
+			} else if len(coworkers) >= 2 {
+				rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+				rnd.Shuffle(len(coworkers), func(i, j int) { coworkers[i], coworkers[j] = coworkers[j], coworkers[i] })
+
+				reviewer1 = &(coworkers[0].ID)
+				reviewer2 = &(coworkers[1].ID)
+			}
+		}
+	}
+
+	return s.storage.AddPullRequest(ctx, title, authorId, isOpened, reviewer1, reviewer2)
 }
 
 func (s *PullRequestServiceImpl) GetPullRequest(ctx context.Context, id int64) (core.PullRequest, error) {
@@ -35,4 +55,54 @@ func (s *PullRequestServiceImpl) GetPullRequest(ctx context.Context, id int64) (
 		return core.PullRequest{}, err
 	}
 	return pr, nil
+}
+
+func (s *PullRequestServiceImpl) MergePullRequest(ctx context.Context, id int64) error {
+	return s.storage.MergePullRequest(ctx, id)
+}
+
+// numReviwer = 1 or 2
+func (s *PullRequestServiceImpl) ChangeReviewer(ctx context.Context, pullReqId int64, numReviwer int64) error {
+	pr, err := s.storage.GetPullRequestByID(ctx, pullReqId)
+	if err != nil {
+		return err
+	}
+	coworkers, err := s.storage.GetActiveCoworkers(ctx, pr.AuthorID)
+	if err != nil {
+		return err
+	}
+
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	rnd.Shuffle(len(coworkers), func(i, j int) { coworkers[i], coworkers[j] = coworkers[j], coworkers[i] })
+
+	for _, u := range coworkers {
+		if pr.Reviewer1ID.Valid && u.ID == pr.Reviewer1ID.Int64 {
+			continue
+		}
+		if pr.Reviewer2ID.Valid && u.ID == pr.Reviewer2ID.Int64 {
+			continue
+		}
+
+		switch numReviwer {
+		case 1:
+			return s.storage.ChangeReviewer1(ctx, pullReqId, u.ID)
+		case 2:
+			return s.storage.ChangeReviewer2(ctx, pullReqId, u.ID)
+		}
+	}
+
+	switch numReviwer {
+	case 1:
+		err = s.storage.ResetReviewer1(ctx, pullReqId)
+		if err != nil {
+			return err
+		}
+	case 2:
+		err = s.storage.ResetReviewer2(ctx, pullReqId)
+		if err != nil {
+			return err
+		}
+	}
+
+	return errors.New("no available coworkers to assign as reviewer. Old reviewer was cleared")
 }
--- internal/storage/pull_req_repo.go
+++ internal/storage/pull_req_repo.go
@@ -55,7 +55,7 @@ func (db *DB) MergePullRequest(ctx context.Context, id int64) error {
 	return nil
 }
 
-func (db *DB) ResetReviewer1(ctx context.Context, pullReqId int64, newReviewerId int64) error {
+func (db *DB) ChangeReviewer1(ctx context.Context, pullReqId int64, newReviewerId int64) error {
 	_, err := db.conn.ExecContext(
 		ctx,
 		`UPDATE pull_requests SET reviewer1_id = $1 WHERE id = $2`,
@@ -69,7 +69,7 @@ func (db *DB) ResetReviewer1(ctx context.Context, pullReqId int64, newReviewerId
 	return nil
 }
 
-func (db *DB) ResetReviewer2(ctx context.Context, pullReqId int64, newReviewerId int64) error {
+func (db *DB) ChangeReviewer2(ctx context.Context, pullReqId int64, newReviewerId int64) error {
 	_, err := db.conn.ExecContext(
 		ctx,
 		`UPDATE pull_requests SET reviewer2_id = $1 WHERE id = $2`,
@@ -80,5 +80,33 @@ func (db *DB) ResetReviewer2(ctx context.Context, pullReqId int64, newReviewerId
 		return err
 	}
 	db.log.Info("reviewer2 reset", "pull_request_id", pullReqId, "new_reviewer_id", newReviewerId)
+	return nil
+}
+
+func (db *DB) ResetReviewer1(ctx context.Context, pullReqId int64) error {
+	_, err := db.conn.ExecContext(
+		ctx,
+		`UPDATE pull_requests SET reviewer1_id = NULL WHERE id = $1`,
+		pullReqId,
+	)
+	if err != nil {
+		db.log.Error("failed to reset reviewer1", "pull_request_id", pullReqId, "error", err)
+		return err
+	}
+	db.log.Info("reviewer1 reset", "pull_request_id", pullReqId)
+	return nil
+}
+
+func (db *DB) ResetReviewer2(ctx context.Context, pullReqId int64) error {
+	_, err := db.conn.ExecContext(
+		ctx,
+		`UPDATE pull_requests SET reviewer2_id = NULL WHERE id = $1`,
+		pullReqId,
+	)
+	if err != nil {
+		db.log.Error("failed to reset reviewer2", "pull_request_id", pullReqId, "error", err)
+		return err
+	}
+	db.log.Info("reviewer2 reset", "pull_request_id", pullReqId)
 	return nil
 }
